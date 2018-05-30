c740a8833932d6c1a5f0d20481fd80f208453813
Daniel Obembe <ayo.obembe@gmail.com>
2018-05-30 18:00:37 -0500

    created functionality for creating and displaying blog posts

--- app/routes.py
+++ app/routes.py
@@ -12,27 +12,22 @@ from app import db
 from app.forms import RegistrationForm
 from datetime import datetime
 from app.forms import EditProfileForm
+from app.forms import PostForm 
+from app.models import Post
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET','POST'])
+@app.route('/index', methods=['GET','POST'])
 @login_required
 def index():
-    user = {'username': 'Miguelle'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The avengers movie was so cool'
-        },
-        {
-            'author': {'username': 'Tamara'},
-            'body': 'I would like to move to a new city!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
