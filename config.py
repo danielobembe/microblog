16398796f7845fd17762e0e877b8ddf8d77fe21a
Daniel Obembe <ayo.obembe@gmail.com>
2018-05-31 19:45:31 -0500

    included pagination capability on back-end

--- config.py
+++ config.py
@@ -12,5 +12,7 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS =    ['obembe.dan@gmail.com']
+    POSTS_PER_PAGE = 3
+    
 
     
