4bd7e02ba58de1c615f32ccaf2886cd7ed1aead6
Daniel Obembe <ayo.obembe@gmail.com>
2018-06-05 20:22:17 -0500

    included centralized timezone and local time display on front end

--- app/email.py
+++ app/email.py
@@ -8,7 +8,7 @@ def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
-def send_mail(subject, sender, recipients, text_body, html_body):
+def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body 
     msg.html = html_body 
