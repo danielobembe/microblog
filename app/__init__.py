dbc7b8d5d62eee9240f171b67cc4648a1887d1d2
Daniel Obembe <ayo.obembe@gmail.com>
2018-06-07 22:45:21 -0500

    Marking text for lanuage translation by flask-babel

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,10 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
+from flask_babel import _
+from flask_babel import lazy_gettext as _l
 
 
 app = Flask(__name__)
@@ -18,13 +22,13 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 
-from app import routes, models
-from app import routes, models, errors
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -54,3 +58,9 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+from app import routes, models, errors
